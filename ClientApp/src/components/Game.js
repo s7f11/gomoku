4c7cd075d3a5a3477c368406f369a8316846a187
s7f11 <92713405+s7f11@users.noreply.github.com>
2023-09-17 00:02:16 +0900

    Update Game.js

--- ClientApp/src/components/Game.js
+++ ClientApp/src/components/Game.js
@@ -2,27 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+// 'firebase' パッケージからFirebaseアプリモジュールをインポート
 import firebase from 'firebase/compat/app';
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebaseを初期化
 const firebaseConfig = {
-  apiKey: "AIzaSyDR5RHtYnqrVFczxk5JgDP3R6zD5SJVLvU",
-  authDomain: "gomoku-ad695.firebaseapp.com",
-  projectId: "gomoku-ad695",
-  storageBucket: "gomoku-ad695.appspot.com",
-  messagingSenderId: "1094967554954",
-  appId: "1:1094967554954:web:1274f002a045de64360e73",
-  measurementId: "G-TK62D1JYM2"
+  apiKey: 'YOUR_API_KEY',
+  authDomain: 'YOUR_AUTH_DOMAIN',
+  projectId: 'YOUR_PROJECT_ID',
+  storageBucket: 'YOUR_STORAGE_BUCKET',
+  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+  appId: 'YOUR_APP_ID',
 };
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+const app = firebase.initializeApp(firebaseConfig);
 
 const roomNumber = sessionStorage.getItem('input_number');
 const db = firebase.firestore();
